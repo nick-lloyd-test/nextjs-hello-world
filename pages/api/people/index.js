5f8494bc1c9986876a3af623f4e0f245810a2911
nick-lloyd-test <137975460+nick-lloyd-test@users.noreply.github.com>
2023-06-28 16:01:11 +1000

    Update index.js

--- pages/api/people/index.js
+++ pages/api/people/index.js
@@ -5,10 +5,10 @@ export default function handler(_req, res) {
   console.log('Trying here')
 
   fetch('https://test-nodejs-qzou.onrender.com/api')
-  .then((response) => response.json())
-  .then((data) => {
-    return res.status(200).send(data)  
-  })
+  .then((response) => res.send(response))
+  // .then((data) => {
+  //   return res.status(200).send(data)  
+  // })
   .error((err) => res.status(500).send('Nope ' + err))
 
   console.log('Fetched')
