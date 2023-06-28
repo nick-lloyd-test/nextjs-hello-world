421518bf32f384fd449eebc0e86a12ac791c2bcc
nick-lloyd-test <137975460+nick-lloyd-test@users.noreply.github.com>
2023-06-28 15:50:04 +1000

    Update index.js

--- pages/api/people/index.js
+++ pages/api/people/index.js
@@ -2,11 +2,11 @@ import { NextApiResponse, NextApiRequest } from 'next'
 
 export default function handler(_req, res) {
 
-  // fetch('test-nodejs/api')
-  // .then((response) => response.json())
-  // .then((data) => {
-  //   return res.status(200).send(data)  
-  // });
+  fetch('test-nodejs-qzou.onrender.com/api')
+  .then((response) => response.json())
+  .then((data) => {
+    return res.status(200).send(data)  
+  });
 
-  return res.status(200).send({people: ['Yes']})
+  //return res.status(200).send({people: ['Yes']})
 }
