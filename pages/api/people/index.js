aea544eb0c2259ad256e6c2c2728ae7acbf066e6
nick-lloyd-test <137975460+nick-lloyd-test@users.noreply.github.com>
2023-06-28 15:44:31 +1000

    Update fetch

--- pages/api/people/index.js
+++ pages/api/people/index.js
@@ -2,7 +2,7 @@ import { NextApiResponse, NextApiRequest } from 'next'
 
 export default function handler(_req, res) {
 
-  fetch('https://test-nodejs/api')
+  fetch('test-nodejs/api')
   .then((response) => response.json())
   .then((data) => {
     return res.status(200).send(data)  
