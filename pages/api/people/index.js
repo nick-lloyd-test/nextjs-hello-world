992a3af745fd32377f8e87b8a558fb9c359e4336
nick-lloyd-test <137975460+nick-lloyd-test@users.noreply.github.com>
2023-06-28 15:02:26 +1000

    Removed ts stuff

--- pages/api/people/index.js
+++ pages/api/people/index.js
@@ -1,5 +1,5 @@
 import { NextApiResponse, NextApiRequest } from 'next'
 
-export default function handler(_req: NextApiRequest, res: NextApiResponse) {
+export default function handler(_req, res) {
   return res.status(200).json({peoples: ['John'])
 }
